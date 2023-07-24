2967ad33fbc3dac56f28700d72875e9d690430ac
Dror Granot <81293610+DrorGr@users.noreply.github.com>
2023-07-24 16:27:19 +0300

    init

---
+++ public/src/Components/Form.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { Grid, TextField } from "@material-ui/core";
+
+const MyForm = ({ onSubmit, initialFormData }) => {
+  const [formData, setFormData] = useState(initialFormData);
+
+  useEffect(() => {
+    onSubmit(formData);
+  }, [formData, onSubmit]);
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <form style={{ padding: "10px" }}>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={12} xl={3}>
+          <TextField
+            name="name"
+            label="Name"
+            type="name"
+            // variant="outlined"
+            value={formData.name}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            autoFocus
+            InputLabelProps={{ style: { color: "#071f2a" } }}
+            InputProps={{ style: { color: "#071f2a" } }}
+          />
+        </Grid>
+        <Grid item xs={12} xl={3}>
+          <TextField
+            name="email"
+            label="Email"
+            type="email"
+            // variant="outlined"
+            value={formData.email}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            InputLabelProps={{ style: { color: "#071f2a" } }}
+            InputProps={{ style: { color: "#071f2a" } }}
+          />
+        </Grid>
+        <Grid item xs={12} xl={3}>
+          <TextField
+            name="phone"
+            label="Phone"
+            type="tel"
+            // variant="outlined"
+            value={formData.phone}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            InputLabelProps={{ style: { color: "#071f2a" } }}
+            InputProps={{ style: { color: "#071f2a" } }}
+          />
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default MyForm;
